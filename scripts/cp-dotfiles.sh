fe230837544c4f9dbc838a7e5405d27e5b6868b4
Pratham Patel <thefirst1322@gmail.com>
2023-01-10 09:51:43 +0530

    neovim: vim-plug

--- scripts/cp-dotfiles.sh
+++ scripts/cp-dotfiles.sh
@@ -23,3 +23,6 @@ rm -rf dotfiles
 
 popd
 popd
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
