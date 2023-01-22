124e49c89394f9073a39fcbcbf054ae6c85a69e0
Pratham Patel <thefirst1322@gmail.com>
2023-01-22 09:53:52 +0530

    add checkhealth for telescope plugin as mentioned by the project README

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -27,5 +27,8 @@ popd
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 nvim +'PlugInstall' +'q' +'q'
+nvim +'checkhealth telescope' +'q' +'q'
+
 popd
