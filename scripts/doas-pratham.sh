fa81a6340e4a572ae2aa3f016385cc960ff7fd7d
Pratham Patel <thefirst1322@gmail.com>
2023-02-10 13:48:20 +0530

    neovim: switch from vim-plug to Packer.nvim

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -25,10 +25,5 @@ popd
 # NEOVIM PLUGINS
 ################################################################################
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim +'PlugInstall' +'q' +'q'
-nvim +'checkhealth telescope' +'q' +'q'
-
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 popd
