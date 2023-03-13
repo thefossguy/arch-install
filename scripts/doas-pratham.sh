5eb77c41ae30216d5e81563e19f051b64b6d69dd
Pratham Patel <thefirst1322@gmail.com>
2023-03-13 16:33:38 +0530

    doas-pratham: do not copy dir '_OTHER'

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -10,7 +10,8 @@ rsync \
     --verbose --recursive --size-only --human-readable \
     --progress --stats \
     --itemize-changes --checksum --perms \
-    --exclude=".git" --exclude=".gitignore" --exclude="README.md" --exclude="run_me.sh" \
+    --exclude=".git" --exclude=".gitignore" --exclude="README.md" \
+    --exclude="run_me.sh" --exclude="_OTHER" \
     ../dotfiles/ ~/
 popd
 popd
