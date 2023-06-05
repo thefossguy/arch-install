b85dfd480b03342404838693d0202c6ed9eb4209
Pratham Patel <thefirst1322@gmail.com>
2023-06-05 20:22:16 +0530

    doas-pratham: force checkout the dotfiles repo
    When doing 'git checkout' for the dotfiles repo, the '.bashrc' file is
    already present (curtsy of the 'bash' package on Arch Linux and I reckon
    on other distros too). Therefore, use the '-f' flag with the 'git checkout'
    command.
    
    Since this is a fresh installation, the force checkout shouldn't hurt.

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -3,5 +3,5 @@ PRATHAMS_HOME=/home/pratham
 
 pushd ${PRATHAMS_HOME}
 git clone --depth 1 --bare https://git.thefossguy.com/thefossguy/dotfiles.git ${PRATHAMS_HOME}/.dotfiles
-git --git-dir=${PRATHAMS_HOME}/.dotfiles --work-tree=${PRATHAMS_HOME} checkout
+git --git-dir=${PRATHAMS_HOME}/.dotfiles --work-tree=${PRATHAMS_HOME} checkout -f
 popd
