05524bb8ba2207b32fb146d4ec81a8a8b41c8316
Pratham Patel <thefirst1322@gmail.com>
2023-06-05 20:02:35 +0530

    doas-pratham: fix the dotfiles directory path

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-PRATHAM_HOME=/home/pratham
+PRATHAMS_HOME=/home/pratham
 
-pushd ${PRATHAM_HOME}
-git clone --depth 1 --bare https://git.thefossguy.com/thefossguy/dotfiles.git
-git --git-dir=${PRATHAM_HOME}/my-git-repos/dotfiles --work-tree=${PRATHAM_HOME} checkout
+pushd ${PRATHAMS_HOME}
+git clone --depth 1 --bare https://git.thefossguy.com/thefossguy/dotfiles.git ${PRATHAMS_HOME}/.dotfiles
+git --git-dir=${PRATHAMS_HOME}/.dotfiles --work-tree=${PRATHAMS_HOME} checkout
 popd
