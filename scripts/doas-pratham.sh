8bd2ffafddc3d45c8f4f5181401b829919940393
Pratham Patel <thefirst1322@gmail.com>
2023-06-05 19:00:49 +0530

    try moving to the bare repo ;)

--- scripts/doas-pratham.sh
+++ scripts/doas-pratham.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
+PRATHAM_HOME=/home/pratham
 
-pushd /home/pratham
-mkdir my-git-repos
-pushd my-git-repos
-git clone --depth 1 https://git.thefossguy.com/thefossguy/dotfiles.git
-pushd dotfiles
-tput -x clear
-rsync \
-    --verbose --recursive --size-only --human-readable \
-    --progress --stats \
-    --itemize-changes --checksum --perms \
-    --exclude=".git" --exclude=".gitignore" --exclude="README.md" \
-    --exclude="run_me.sh" --exclude="_OTHER" \
-    ../dotfiles/ ~/
-popd
-popd
+pushd ${PRATHAM_HOME}
+git clone --depth 1 --bare https://git.thefossguy.com/thefossguy/dotfiles.git
+git --git-dir=${PRATHAM_HOME}/my-git-repos/dotfiles --work-tree=${PRATHAM_HOME} checkout
 popd
